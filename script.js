672a263197d7d8844f0f0d4c0ca4cb9217aa8eac
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-07-17 07:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let inps = document.querySelectorAll("input");
+
 function getFormvalue() {
     //Write your code here
-
+    let fname = inps[0].value.trim();
+    let lname = inps[1].value.trim();
+    return alert(`${fname} ${lname}`);
 }
+
+inps[2].addEventListener('submit', getFormvalue);
